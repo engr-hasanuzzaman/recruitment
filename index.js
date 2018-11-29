f4e0bf44581d7e9e1bd92f360da377273c36791f
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-11-29 15:04:00 +0600

    use nodemon for auto code reloading

--- index.js
+++ index.js
@@ -2,28 +2,37 @@
 
 const Hapi = require('hapi');
 const server = Hapi.server({
-  host: 'localhost',
-  port: 8080
+    host: 'localhost',
+    port: 8080
 });
 
-server.rules({
-  method:'GET',
-  path:'/hello',
-  handler:function(request,h) {
-
-      return'hello world';
-  }
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function(request, h) {
+        request.log('with in handler');
+        return 'hello world';
+    }
 })
 
-async function start(){
-  try {
-    await server.start();
-  } catch (err) {
-    console.log('encounter err that is ', err);
-    process.exit(1);
-  }
+server.route({
+    method: 'GET',
+    path: '/posts',
+    handler: function(req, h) {
+        const posts = [{ title: 'Name', age: 23 }, { title: 'Name', age: 23 }, { title: 'Name', age: 23 }]
+        return posts;
+    }
+})
 
-  console.log('server running at: ', server.info.uri);
+async function start() {
+    try {
+        await server.start();
+    } catch (err) {
+        console.log('encounter err that is ', err);
+        process.exit(1);
+    }
+
+    console.log('server running at: ', server.info.uri);
 }
 
 start();
